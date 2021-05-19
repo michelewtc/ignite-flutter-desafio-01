235e60f3d89bd11935c4b75e6995cd6e55733f0b
michelewtc <michelechan1996@gmail.com>
2021-05-19 00:32:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# meu_primeiro_app
+
+Desafio 01 - Estrutura da aplicação<br />
+
+Nesse desafio você deverá reestruturar o aplicativo construído durante as aulas, separando os widgets que estão no arquivo `main.dart` afim de melhorar a compreensão do código, isolando as responsabilidades em diferentes arquivos.
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Meu Primeiro App",
+      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.deepPurple,
+        ),
+      ),
+      darkTheme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.grey,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  var count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contador"),
+        centerTitle: true,
+      ),
+      body: Center(child: Text(
+        "Contagem: $count", 
+        style: TextStyle(
+          color: Colors.white, 
+          fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
